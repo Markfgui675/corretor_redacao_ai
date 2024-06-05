0af3a1c4eeb88efef5242ababd73cfe055f73122
Marcos Fellipe Guimarães <marcosfgui2006@gmail.com>
2024-06-05 19:19:40 -0300

    atuaização da ia

--- corretor_ia/inteligencia/corretor.py
+++ corretor_ia/inteligencia/corretor.py
@@ -20,7 +20,7 @@ def corretor_redacao(redacao: str, user: User) -> RedacaoComentario:
     # Set up the model
     generation_config = {
         "temperature": 0.95,
-        "top_p": 0.95,
+        "top_p": 0.9,
         "top_k": 64,
         "max_output_tokens": 8192,
         "response_mime_type": "text/plain",
